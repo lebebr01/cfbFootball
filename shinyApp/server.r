962fdcd8243291194ba5cbb8e9a452048c6855e2
Lobo <lebebr01@gmail.com>
2013-03-26 09:22:15 -0500

    updated shiny app

--- shinyApp/server.r
+++ shinyApp/server.r
@@ -1,134 +1,55 @@
 library(ggplot2)
+library(proto)
+library(scales)
+library(RColorBrewer)
 
 load("MinnFootball.Rdata")
 
-geom_boxplot <- function (mapping = NULL, data = NULL, stat = "boxplot", position = "dodge",
-                          outlier.colour = NULL, outlier.shape = NULL, outlier.size = NULL,
-                          notch = FALSE, notchwidth = .5, ...) {
-  outlier_defaults <- Geom$find('point')$default_aes()
-  outlier.colour <- outlier.colour %||% outlier_defaults$colour
-  outlier.shape <- outlier.shape %||% outlier_defaults$shape
-  outlier.size <- outlier.size %||% outlier_defaults$size
-  GeomBoxplot$new(mapping = mapping, data = data, stat = stat, 
-                  position = position, outlier.colour = outlier.colour, outlier.shape = outlier.shape, 
-                  outlier.size = outlier.size, notch = notch, notchwidth = notchwidth, ...)
+# Returns a logical vector of which values in `x` are within the min and max
+# values of `range`.
+in_range <- function(x, range) {
+  x >= min(range) & x <= max(range)
 }
 
-GeomBoxplot <- proto(Geom, {
-  objname <- "boxplot"
-  
-  reparameterise <- function(., df, params) {
-    df$width <- df$width %||% 
-      params$width %||% (resolution(df$x, FALSE) * 0.9)
-    
-    if (!is.null(df$outliers)) {
-      suppressWarnings({
-        out_min <- vapply(df$outliers, min, numeric(1))
-        out_max <- vapply(df$outliers, max, numeric(1))
-      })
-      
-      df$ymin_final <- pmin(out_min, df$ymin)
-      df$ymax_final <- pmax(out_max, df$ymax)
-    } 
-    
-    transform(df,
-              xmin = x - width / 2, xmax = x + width / 2, width = NULL
-    )
-    
-  }
-  
-  draw <- function(., data, ..., fatten = 2, outlier.colour = NULL, outlier.shape = NULL, outlier.size = 2,
-                   notch = FALSE, notchwidth = .5) { 
-    common <- data.frame(
-      colour = data$colour, 
-      size = data$size, 
-      linetype = data$linetype,
-      fill = alpha(data$fill, data$alpha),  
-      group = data$group,
-      stringsAsFactors = FALSE
-    )
-    
-    whiskers <- data.frame(
-      x = data$x,
-      xend = data$x, 
-      y = c(data$upper, data$lower), 
-      yend = c(data$ymax, data$ymin),
-      alpha = NA,
-      common)
-    
-    box <- data.frame(
-      xmin = data$xmin, 
-      xmax = data$xmax, 
-      ymin = data$lower, 
-      y = data$middle, 
-      ymax = data$upper,
-      ynotchlower = ifelse(notch, data$notchlower, NA),
-      ynotchupper = ifelse(notch, data$notchupper, NA),
-      notchwidth = notchwidth,
-      alpha = data$alpha, 
-      common)
-    
-    if (!is.null(data$outliers) && length(data$outliers[[1]] >= 1)) {
-      outliers <- data.frame(
-        y = data$outliers[[1]],
-        x = data$x[1],
-        colour = outlier.colour %||% data$colour[1],
-        shape = outlier.shape %||% data$shape[1],
-        size = outlier.size %||% data$size[1],
-        fill = NA,
-        alpha = NA,
-        stringsAsFactors = FALSE)
-      outliers_grob <- GeomPoint$draw(outliers, ...)
-    } else {
-      outliers_grob <- NULL
-    }
-    
-    ggname(.$my_name(), grobTree(
-      outliers_grob,
-      GeomSegment$draw(whiskers, ...),
-      GeomCrossbar$draw(box, fatten = fatten, ...)
-    ))
-  }
-  
-  guide_geom <- function(.) "boxplot"  
-  draw_legend <- function(., data, ...)  {
-    data <- aesdefaults(data, .$default_aes(), list(...))
-    gp <- with(data, gpar(col=colour, fill=alpha(fill, alpha), lwd=size * .pt, lty = linetype))
-    gTree(gp = gp, children = gList(
-      linesGrob(0.5, c(0.1, 0.25)),
-      linesGrob(0.5, c(0.75, 0.9)),
-      rectGrob(height=0.5, width=0.75),
-      linesGrob(c(0.125, 0.875), 0.5)
-    ))
-  }
-  
-  default_stat <- function(.) StatBoxplot
-  default_pos <- function(.) PositionDodge
-  default_aes <- function(.) aes(weight=1, colour="grey20", fill="white", size=0.5, alpha = NA, shape = 16, linetype = "solid")
-  required_aes <- c("x", "lower", "upper", "middle", "ymin", "ymax")
-  
-})
+#minnFootball$Decade <- with(minnFootball, ifelse(Year %in% 1880:1889, 1, ifelse(
+#  Year %in% 1890:1899, 2, ifelse(Year %in% 1900:1909, 3, ifelse(
+#    Year %in% 1910:1919, 4, ifelse(Year %in% 1920:1929, 5, ifelse(
+#      Year %in% 1930:1939, 6, ifelse(Year %in% 1940:1949, 7, ifelse(
+#        Year %in% 1950:1959, 8, ifelse(Year %in% 1960:1969, 9, ifelse(
+#          Year %in% 1970:1979, 10, ifelse(Year %in% 1980:1989, 11, ifelse(
+#            Year %in% 1990:1999, 12, ifelse(Year %in% 2000:2009, 13, 14))))))))))))))
 
 shinyServer(function(input, output) {
   
-    output$main_plot <- renderPlot({
+  subsetData <- function(){
+    ## Years to subset on
+    years.limits <- as.numeric(unlist(strsplit(paste(input$yr.sub, 
+                                                     collapse = ' '), ' ')))
+    years.sub <- seq(years.limits[1], years.limits[2], by = 1)
     
     ## Limit Range of data to a specific decade
-    minnFootball.sub <- subset(minnFootball, Year %in% input$yr.sub)
+    minnFootball.sub <- subset(minnFootball, Year %in% years.sub)
+    minnFootball.sub
     
+  }
+  
+    output$main_plot <- renderPlot({
+      
+      minnFootball.sub <- subsetData()     
+      
     ## label Win/loss
     if(input$W.L){
-      aes.map.point <- aes_string(color = "W.L", shape = "W.L")
+      aes.map.point <- geom_jitter(mapping = aes_string(color = "W.L", shape = "W.L"))
       manual.color <- scale_color_brewer(palette = "Set1")
     } else {
-      aes.map.point <- aes_string()
-      manual.color <- ""
+      aes.map.point <- geom_jitter()
+      manual.color <- NULL
     }
     
     
     ## draw base plot
     p <- ggplot(minnFootball.sub, aes(x = factor(Year), y = PF)) + 
-      geom_boxplot() + theme_bw() + geom_jitter(mapping = aes.map.point) + 
+      geom_boxplot() + theme_bw() + aes.map.point + 
       xlab("Year") + manual.color
     print(p)
   })
--- shinyApp/ui.r
+++ shinyApp/ui.r
@@ -6,24 +6,11 @@ shinyUI(pageWithSidebar(
   
   sidebarPanel(
     
-    wellPanel(
-      selectInput(inputId = "yr.sub",
-                  label = "Decades",
-                  choices = c("1880's" = 1880:1889,
-                              "1890's" = 1890:1899,
-                              "1900's" = 1900:1909,
-                              "1910's" = 1910:1919,
-                              "1920's" = 1920:1929,
-                              "1930's" = 1930:1939,
-                              "1940's" = 1940:1949,
-                              "1950's" = 1950:1959,
-                              "1960's" = 1960:1969,
-                              "1970's" = 1970:1979,
-                              "1980's" = 1980:1989,
-                              "1990's" = 1990:1999,
-                              "2000's" = 2000:2009,
-                              "2010's" = 2010:2019),
-                   selected = "2010's")
+    sliderInput(inputId = "yr.sub", 
+                label = "Years",
+                min = 1882, 
+                max = 2012,
+                value = c(2000, 2009)      
       ),
     
     wellPanel(
@@ -32,15 +19,18 @@ shinyUI(pageWithSidebar(
                   choices = c("Points Scored" = "PF",
                               "Points Against" = "PA"),
                   selected = "Points Scored")
-      )
+      ),
     
     checkboxInput(inputId = "W.L",
                   label = "Identify Win/Loss",
-                  value = FALSE)
+                  value = FALSE),
+    
+    submitButton("Update Graph")
+    
     ),
   
   mainPanel(
-    plotOutput(outputId = "main_plot"),
+    plotOutput(outputId = "main_plot")
     )
   
   ))

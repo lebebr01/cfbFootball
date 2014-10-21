6f968ead820b4b9d82c117c2722d264f03a82b51
Brandon LeBeau <lebebr01@gmail.com>
2014-10-21 11:39:27 -0500

    analysis scripts

---
+++ R/Analysis/coachIRT.r
@@ -0,0 +1,119 @@
+library(ltm)
+library(reshape2)
+library(data.table)
+library(lavaan)
+library(semPlot)
+
+# loading relevant file - must be in root of cfbFootball directory
+source(paste(getwd(), '/R/mergeCoach.R', sep = ''))
+
+# removing duplicates due to merging
+ybyCoach <- data.table(ybyCoach)
+
+# subset to include only 2013
+#ybyCoach <- ybyCoach[i = Year %in% 1980:2013]
+ybyCoach <- unique(ybyCoach, by = c('Year', 'Team', 'coach'))
+
+# removing coaches with less than 2 games for team
+ybyCoach[, numGames := Win + Loss + Tie, by = list(Year, Team, coach)]
+ybyCoach <- ybyCoach[i = numGames > 2]
+
+# removing cases with NA for conference
+ybyCoach <- ybyCoach[complete.cases(ybyCoach$Conference),]
+
+# dichotomizing conference variable into dummy variables
+ybyCoach$aaConf <- ifelse(ybyCoach$Conference == "American Athletic Conference", 1, 0)
+ybyCoach$acConf <- ifelse(ybyCoach$Conference == "Atlantic Coast Conference", 1, 0)
+ybyCoach$b12Conf <- ifelse(ybyCoach$Conference == "Big 12 Conference", 1, 0)
+ybyCoach$b10Conf <- ifelse(ybyCoach$Conference == "Big Ten Conference", 1, 0)
+ybyCoach$cusaConf <- ifelse(ybyCoach$Conference == "Conference USA", 1, 0)
+ybyCoach$maConf <- ifelse(ybyCoach$Conference == "Mid-American Conference", 1, 0)
+ybyCoach$mwConf <- ifelse(ybyCoach$Conference == "Mountain West Conference", 1, 0)
+ybyCoach$p12Conf <- ifelse(ybyCoach$Conference == "Pacific-12 Conference", 1, 0)
+ybyCoach$secConf <- ifelse(ybyCoach$Conference == "Southeastern Conference", 1, 0)
+ybyCoach$sbConf <- ifelse(ybyCoach$Conference == "Sun Belt Conference", 1, 0)
+
+# power 5 conference indicator
+ybyCoach$power5conf <- ifelse(ybyCoach$Conference %in% 
+                                c('Atlantic Coast Conference', 
+                                  'Big 12 Conference', 'Big Ten Conference',
+                                  'Pacific-12 Conference', 'Southeastern Conference'),
+                              1, 0)
+
+# number of all americans - if missing put 0
+ybyCoach$numAA <- ifelse(is.na(ybyCoach$numAA), 0, ybyCoach$numAA)
+
+# create bowl eligible variable
+ybyCoach$bowlElig <- ifelse(ybyCoach$Win >= 6, 1, 0)
+
+# merge in yby data
+ybyCoach <- left_join(ybyCoach, yby, by = c("Year", "Team"))
+
+# convert W/L/T into 1/0 variable
+ybyCoach$wingbg <- ifelse(ybyCoach$WL == 'W', 1, 0)
+
+# create ID variable within year, team, and coach
+ybyCoach$ID <- id(ybyCoach[c("Year", "Team", "coach")])
+ybyCoach <- data.table(ybyCoach)
+ybyCoach$wID <- ybyCoach[, wID := 1:length(wingbg), by = list(ID)]
+
+# loop through years creating and fit rasch model within each year to coaches
+year <- subset(unique(ybyCoach$Year), unique(ybyCoach$Year) > 1930)
+raschVal <- vector("list", length(year))
+for(i in 1:length(unique(ybyCoach$Year))) {
+  x <- subset(ybyCoach, Year == year[i])
+  # data structure for rasch model
+  xw <- data.frame(reshape(subset(x, select = c(wID, coach, wingbg)), timevar = "coach", 
+                                   idvar = "wID", direction = "wide"))
+  raschVal[[i]] <- data.frame(rasch(subset(xw, select = -wID),
+                                    constraint = cbind(ncol(xw), 1), control = list(GHk = 15))$coefficients,
+                              year = year[i],
+                              Team = unique(x, by = c('Team', 'coach'))$Team)
+}
+
+raschValC <- do.call('rbind', raschVal)
+raschValC$coach2 <- gsub('\\.+', ' ', gsub('wingbg\\.', '', rownames(raschValC)))
+raschValC$coach2 <- gsub("\\d+$", "", raschValC$coach2)
+setnames(raschValC, 'year', 'Year')
+
+# removing punctuation from ybyCoach coach name
+ybyCoach$coach2 <- gsub('[[:punct:]]', '', ybyCoach$coach)
+
+# one row per coach, year, and team
+ybyCoach2 <- unique(ybyCoach, by = c('Year', 'Team', 'coach'))
+
+# merge raschValC with ybyCoach.
+library(dplyr)
+raschValC2 <- left_join(raschValC, ybyCoach2, by = c('Team', 'Year', 'coach2'))
+raschValC2 <- arrange(raschValC2, coach2, Year)
+
+# adjusting year to start at 0
+coaches <- unique(raschValC2$coach2)
+year2 <- vector("list", length(coaches))
+for(i in 1:length(coaches)) {
+  year2[[i]] <- subset(raschValC2, coach2 == coaches[i])$Year - min(subset(raschValC2, coach2 == coaches[i])$Year)
+}
+
+raschValC2$Year2 <- do.call("c", year2)
+
+# plotting coach ability level
+library(ggplot2)
+l <- ggplot(raschValC2, aes(x = Year2, y = beta.i, group = coach2)) + theme_bw()
+l + geom_line()
+
+l <- ggplot(subset(raschValC2, power5conf == 1), aes(x = Year2, y = beta.i)) + theme_bw()
+l + geom_line(aes(group = coach2)) + coord_cartesian(ylim = c(-4, 4)) + geom_smooth()
+
+# modeling coach ability level
+library(lme4)
+simp.mod <- lmer(beta.i ~ 1 + Year2 + (1 + Year2|coach2), data = raschValC2)
+quad.mod <- lmer(beta.i ~ 1 + Year2 + I(Year2^2) + (1 + Year2|coach2), data = raschValC2)
+
+
+raschValC2$overWinmc <- scale(raschValC2$overWin, center = TRUE, scale = FALSE)
+raschValC2$alltimemc <- scale(raschValC2$alltime, center = TRUE, scale = TRUE)
+raschValC2$numGamesmc <- scale(raschValC2$numGames, center = TRUE, scale = FALSE)
+quad.mod.pred <- lmer(beta.i ~ 1 + Year2 + overWinmc + Year2:overWinmc + 
+                        alltimemc + Year2:alltimemc + numAA + Year2:numAA + 
+                        numGamesmc + numGamesmc:Year2 + power5conf + Year2:power5conf + 
+                        (1 + Year2|coach2), data = raschValC2)
